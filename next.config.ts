6a2c71004fc67dba2863faa030ff07e7123f938c
Krisna Harsya Saputra <129250588+kidsharsya@users.noreply.github.com>
2025-10-29 22:44:47 +0700

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import createNextPWA from 'next-pwa';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const withPWA = createNextPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+const nextConfig = {
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
