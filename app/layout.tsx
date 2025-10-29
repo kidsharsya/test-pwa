0e4a76252935a3052510df5d513ea1a2df01e712
Krisna Harsya Saputra <129250588+kidsharsya@users.noreply.github.com>
2025-10-29 23:32:14 +0700

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import InstallButton from '@/components/Instal-Button';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,7 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <InstallButton />
+        {children}
+      </body>
     </html>
   );
 }
